a229ac6dc4cce5acc0008db9749c2b551db2f892
selçuk güler <2003022022@student.beykent.edu.tr>
2022-08-10 20:30:42 +0300

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import Footer from '../components/footer'
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Head>
+				<title>Poll</title>
+			</Head>
 			<Component {...pageProps} />
 			<Footer />
 		</>
